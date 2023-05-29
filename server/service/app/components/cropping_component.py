8f14266fc803a8872999a2d7bc1a250dd91c09fd
spv7016 <spv7016@gmail.com>
2023-05-30 01:14:52 +0300

    Рефактор кроп компонента и устранение черных полос

--- server/service/app/components/cropping_component.py
+++ server/service/app/components/cropping_component.py
@@ -1,5 +1,4 @@
 import cv2
-import os
 import numpy as np
 
 from app.components.common.abstract_cropping import Interface
@@ -22,30 +21,18 @@ class Cropping(Interface):
     _filling = None
     _face_cascade = None
     _debug = False
+    _crop_margin: float
     _crop_size_width: int
     _crop_size_height: int
 
     def init(self, params: dict, debug: bool):
         self._params = params
         self._debug = debug
-
-        cascade = params['cascade']
         self._filling = self._params['filling']
-
-        if not os.path.isfile(cascade):
-            raise Exception(f'Файл {cascade} не найден')
-
-        if self._params['crop_size_width'] is None:
-            self._crop_size_width = 0
-        else:
-            self._crop_size_width = int(self._params['crop_size_width'])
-
-        if self._params['crop_size_height'] is None:
-            self._crop_size_height = 0
-        else:
-            self._crop_size_height = int(self._params['crop_size_height'])
-
-        self._face_cascade = cv2.CascadeClassifier(params['cascade'])
+        self._crop_margin = self._params['crop_margin']
+        self._crop_size_width = int(self._params['crop_size_width'])
+        self._crop_size_height = int(self._params['crop_size_width'])
+        self._face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + params['cascade'])
 
     def cropping(self, image: np.ndarray) -> (list, list):
         faces = self._face_cascade.detectMultiScale(image)
@@ -55,34 +42,31 @@ class Cropping(Interface):
             return [], []
         elif len(faces) > 1:
             messages.append('only_one_face')
-        else:
-            for x, y, width, height in faces:
-                if self._filling:
-                    original_image = image.shape[0] * image.shape[1]
-                    cropped_image = (y + height) * (x + width)
-                    if _is_filling(original_image, cropped_image, self._filling):
-                        # bound_rect = iv2.rectangle(frame, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
-                        cropped = image[y:y + height, x:x + width]
-
-                        if self._crop_size_width > 0 and self._crop_size_height > 0:
-                            small = cv2.resize(
-                                cropped,
-                                (self._crop_size_width, self._crop_size_height),
-                                interpolation=cv2.INTER_LINEAR
-                            )
-                        elif self._crop_size_width > 0 and self._crop_size_height == 0:
-                            height = cropped.shape[0]
-                            width = cropped.shape[1]
-                            ratio = self._crop_size_width / width
-                            new_height = int(height * ratio)
-                            small = cv2.resize(
-                                cropped,
-                                (self._crop_size_width, new_height),
-                                interpolation=cv2.INTER_LINEAR
-                            )
-                        else:
-                            small = cv2.resize(cropped, (0, 0), fx=0.5, fy=0.5)
-                    else:
-                        messages.append('come_closer')
-
+        else: # если в кадре найдено только одно лицо
+            face = faces[0]
+            x, y, w, h = face
+            original_image = image.shape[0] * image.shape[1]
+            cropped_image = h * w
+            if not _is_filling(original_image, cropped_image, self._filling):
+                messages.append('come_closer')
+            x1 = x + w
+            y1 = y + h
+            x_center = x + (w // 2)
+            y_center = y + (h // 2)
+            min_dist_to_center = min(x_center,y_center, image.shape[1]-x_center, image.shape[0]-y_center)
+            desired_radius = w//2 * self._crop_margin # желаемая дистанция от центра лица до границы кадра
+            if desired_radius < min_dist_to_center:
+                messages.append('too_close')
+            else:
+                n = min_dist_to_center # дистанция от центра лица до ближайшей границы кадра
+            x = int(x_center - n)
+            y = int(y_center - n)
+            x1 = int(x_center + n)
+            y1 = int(y_center + n)
+            cropped = image[y:y1, x:x1]     
+            small = cv2.resize(
+                cropped, 
+                (self._crop_size_width, self._crop_size_height), 
+                interpolation=cv2.INTER_LINEAR
+                )
         return small, messages
